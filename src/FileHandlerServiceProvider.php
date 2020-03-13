9d93e27855497758813c7cf6026a97a143ea696d
Vince Cole <vince@limeworx.uk>
2020-03-13 12:36:54 +0000

    Adjusted file path - removed .php so that it could find the route to the folder, rather than trying to find a file

--- src/FileHandlerServiceProvider.php
+++ src/FileHandlerServiceProvider.php
@@ -7,8 +7,8 @@ use Illuminate\support\ServiceProvider;
 class FileHandlerServiceProvider extends ServiceProvider{
 
     public function boot(){
-        $this->loadRoutesFrom(__DIR__.'/routes/api.php');
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations.php');
+        $this->loadRoutesFrom(__DIR__.'/routes/api.php'); 
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
 
         $this->publishes([
             __DIR__.'config/image.php'=>config_path('image.php')
