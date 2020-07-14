e7f956b154a1cfd3db692bf968dd36e45d2bc698
Vince Cole <vince@limeworx.uk>
2020-07-14 16:23:21 +0100

    Removed migrations attempting to push mrk 2.0 :D :D :D

--- src/FileHandlerServiceProvider.php
+++ src/FileHandlerServiceProvider.php
@@ -9,7 +9,7 @@ class FileHandlerServiceProvider extends ServiceProvider{
 
     public function boot(){
         $this->loadRoutesFrom(__DIR__.'/routes/api.php'); 
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations'); 
 
         $this->publishes([
             __DIR__.'config/image.php'=>config_path('image.php')
